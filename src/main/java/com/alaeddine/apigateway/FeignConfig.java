c4053ac02816b8f620c0e17b6cdb0ac5d7d9b338
Alaeedeen <a.eloueryemmi@instadeep.com>
2023-06-08 16:04:20 +0100

    disable csrf

--- src/main/java/com/alaeddine/apigateway/FeignConfig.java
+++ src/main/java/com/alaeddine/apigateway/FeignConfig.java
@@ -3,11 +3,20 @@ package com.alaeddine.apigateway;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class FeignConfig {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor("user", "user");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().build();
     }
 }
